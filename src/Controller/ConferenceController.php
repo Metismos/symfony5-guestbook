0b5ea927133dcb1db50ae7476265c90108d6761d
Cillian <cillian@tuimedia.com>
2020-02-20 15:02:36 +0000

    Add db

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
